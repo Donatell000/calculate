72527c44cdd38d1b12a3f2a1c35930ee6ba21c65
Misha Martushev <martushev12@gmail.com>
2024-06-06 17:45:30 +0500

    first ver

---
+++ main.py
@@ -0,0 +1,26 @@
+# n1 - число номер 1
+# n2 - число номер 2
+# op - операция между числами
+
+
+def calculate(n1, n2, op):
+    if op == '+':
+        result = n1 + n2
+    elif op == '-':
+        result = n1 - n2
+    elif op == '*':
+        result = n1 * n2
+    elif op == '/':
+        result = n1 / n2
+    elif op == '^':
+        result = n1 ** n2
+    else:
+        result = None
+    return result
+
+
+num1 = int(input('Введите первое число: '))
+op = input('Введите знак (+,-,*,/,^): ')
+num2 = int(input('Введите второе число: '))
+result = calculate(num1, num2, op)
+print(num1, op, num2, '=', result)
